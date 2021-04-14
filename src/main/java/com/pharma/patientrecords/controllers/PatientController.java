1e334ae0bc712a1755a5499ecc2b718031280b77
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-04-14 11:53:18 +0200

    worked on rabbitmq. patient location saved in location ms

--- src/main/java/com/pharma/patientrecords/controllers/PatientController.java
+++ src/main/java/com/pharma/patientrecords/controllers/PatientController.java
@@ -1,11 +1,15 @@
 package com.pharma.patientrecords.controllers;
 
+import com.google.gson.Gson;
+import com.pharma.patientrecords.messaging.CreateLocationMessage;
 import com.pharma.patientrecords.models.Dossier;
 import com.pharma.patientrecords.models.Patient;
 import com.pharma.patientrecords.models.dto.PatientDto;
 import com.pharma.patientrecords.models.enums.Gender;
 import com.pharma.patientrecords.repositories.DossierRepository;
 import com.pharma.patientrecords.repositories.PatientRepository;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +20,27 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/patients")
 public class PatientController {
-    final PatientRepository patientRepository;
-    final DossierRepository dossierRepository;
+    private final PatientRepository patientRepository;
+    private final DossierRepository dossierRepository;
+    private final AmqpTemplate rabbitTemplate;
+    private final Gson gson;
 
-    public PatientController(PatientRepository patientRepository, DossierRepository dossierRepository) {
+    @Value("${rabbitmq.exchange}")
+    private String exchange;
+    @Value("${rabbitmq.routingKey}")
+    private String routingkey;
+
+    public PatientController(PatientRepository patientRepository, DossierRepository dossierRepository,
+                             AmqpTemplate rabbitTemplate, Gson gson) {
         this.patientRepository = patientRepository;
         this.dossierRepository = dossierRepository;
+        this.rabbitTemplate = rabbitTemplate;
+        this.gson = gson;
+
     }
 
     @PostMapping()
     public ResponseEntity<?> savePatient(@RequestBody PatientDto patientDto) {
-        System.out.println(patientDto);
         Patient p = new Patient();
         p.setFirstName(patientDto.getFirstName());
         p.setLastName(patientDto.getLastName());
@@ -35,11 +49,17 @@ public class PatientController {
         p.setDateOfBirth(patientDto.getDateOfBirth());
         p.setPhoneNumber(patientDto.getPhoneNumber());
         Dossier dossier = new Dossier(patientDto.getDossierInformation());
-        System.out.println(dossier);
         Dossier d = this.dossierRepository.save(dossier);
-        System.out.println(d);
         p.setDossier(d);
-        System.out.println(p);
+        CreateLocationMessage clm = new CreateLocationMessage();
+        clm.setStreetName(patientDto.getStreetName());
+        clm.setHouseNumber(patientDto.getHouseNumber());
+        clm.setZipCode(patientDto.getZipCode());
+        clm.setCity(patientDto.getCity());
+        clm.setCountry(patientDto.getCountry());
+        long locationId = this.saveLocation(clm);
+        System.out.println(locationId);
+        p.setLocationId(locationId);
         return new ResponseEntity<>(patientRepository.save(p), HttpStatus.CREATED);
     }
 
@@ -47,4 +67,13 @@ public class PatientController {
     public ResponseEntity<?> getAll() {
         return new ResponseEntity<>(patientRepository.findAll(), HttpStatus.OK);
     }
+
+    public long saveLocation(CreateLocationMessage clm) {
+        String result = gson.toJson(clm);
+        rabbitTemplate.convertAndSend(exchange, "create-location", result);
+        System.out.println("test");
+        return 1;
+    }
+
+
 }
--- src/main/java/com/pharma/patientrecords/messaging/CreateLocationMessage.java
+++ src/main/java/com/pharma/patientrecords/messaging/CreateLocationMessage.java
@@ -1,4 +1,12 @@
 package com.pharma.patientrecords.messaging;
 
+import lombok.Data;
+
+@Data
 public class CreateLocationMessage {
+    private String streetName;
+    private String houseNumber;
+    private String zipCode;
+    private String city;
+    private String country;
 }
