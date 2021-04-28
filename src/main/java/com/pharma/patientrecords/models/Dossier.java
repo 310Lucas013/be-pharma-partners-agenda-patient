db0ee270d0142c97610b298cdab2033a6bb4642b
i383656 <s.westerburger@student.fontys.nl>
2021-04-28 09:35:59 +0200

    Updated java version from 15 to 16. Removed lombok

--- src/main/java/com/pharma/patientrecords/models/Dossier.java
+++ src/main/java/com/pharma/patientrecords/models/Dossier.java
@@ -1,12 +1,9 @@
 package com.pharma.patientrecords.models;
 
-import lombok.Data;
-
 import javax.persistence.*;
 
 @Entity
 @Table
-@Data
 public class Dossier {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,5 +19,29 @@ public class Dossier {
 
     public Dossier(String information) {
         this.information = information;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
     }
 }
